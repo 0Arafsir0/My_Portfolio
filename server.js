500b8d95a061edbae5d2943ff0380b0b3da4962e
0Arafasir0 <arafsirarafat@gmail.com>
2026-06-11 17:06:26 +0600

    Updated

--- server.js
+++ server.js
@@ -3,23 +3,26 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const express = require('express');
 const app = express();
-require('dotenv').config();
-
 const cors = require("cors");
+require('dotenv').config();
 const dbconfig = require('./config/dbconfig');
 const portfolioRoute = require('./routes/portfolioRoute');
 
 dbconfig.connectionToMongodb();
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://https://myportfolio-gamma-bice-79.vercel.app//"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://myportfolio-12s57a44o-arafsir-s-projects.vercel.app",
+      "https://myportfolio-26gw.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
+app.use(cors(...));  
 app.use(express.json());
 
 app.use('/api/portfolio', portfolioRoute);
